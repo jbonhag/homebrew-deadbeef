0a713cc05c8c4db4bad3625181bb601493bb7a5a
Jeff Bonhag <jeff@thebonhags.com>
2020-05-13 20:52:44 -0400

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# homebrew-deadbeef
+
+This is an unofficial homebrew repository for the
+[DeaDBeeF](https://deadbeef.sourceforge.io/) audio player. It's basically the
+same as `deadbeef-devel` in
+[homebrew-cask](https://github.com/Homebrew/homebrew-cask), but it pins the
+application to a release version which makes it easier to build plugins
+against.
---
+++ deadbeef.rb
@@ -0,0 +1,16 @@
+cask 'deadbeef' do
+  version '1.8.3'
+  sha256 '9cd31ef07b7d1e8e597eed10fd62edc0699d3005f313a23a2bd26fa92b5115df'
+
+  url "https://downloads.sourceforge.net/deadbeef/travis/osx/#{version}/deadbeef-#{version}-osx-x86_64.zip"
+  name 'DeaDBeeF'
+  homepage 'https://deadbeef.sourceforge.io/'
+
+  app 'deadbeef.app'
+
+  zap trash: [
+               '~/Library/Preferences/com.deadbeef.deadbeef.plist',
+               '~/Library/Preferences/deadbeef',
+               '~/Library/Saved Application State/com.deadbeef.deadbeef.savedState',
+             ]
+end
