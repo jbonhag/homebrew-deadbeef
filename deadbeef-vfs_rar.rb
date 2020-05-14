48742c857c9b2e4f6541c0de957b477021bb3951
Jeff Bonhag <jeff@thebonhags.com>
2020-05-13 23:23:08 -0400

    RAR vfs plugin for DeaDBeeF
    This adds a formula for the RAR vfs plugin for DeaDBeeF. I'm not sure if
    there's a good Homebrew-y way to install the plugin to the users' home
    directory, so I added instructions for doing that part manually.

---
+++ deadbeef-vfs_rar.rb
@@ -0,0 +1,32 @@
+class DeadbeefVfsRar < Formula
+  desc "RAR vfs plugin for DeaDBeef player"
+  homepage "https://github.com/DeaDBeeF-Player/vfs_rar"
+  head "https://github.com/DeaDBeeF-Player/vfs_rar.git"
+
+  def install
+    curl "-O", "https://www.rarlab.com/rar/unrarsrc-5.9.2.tar.gz"
+    system "tar", "-xf", "unrarsrc-5.9.2.tar.gz"
+
+    ENV.append "CXXFLAGS", "-I/Applications/DeaDBeeF.app/Contents/Headers"
+
+    inreplace "Makefile" do |s|
+      s.gsub! "$(APP).so", "$(APP).dylib"
+      s.gsub! "~/.local/lib/deadbeef", prefix
+    end
+
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      This plugin needs to be installed like so:
+
+        mkdir -p ~/Library/Application\\ Support/Deadbeef/Plugins
+        cp #{prefix}/vfs_rar.dylib ~/Library/Application\\ Support/Deadbeef/Plugins
+    EOS
+  end
+
+  test do
+    assert_predicate "#{plugins_dir}/vfs_rar.dylib", :exist?
+  end
+end
