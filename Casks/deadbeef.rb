77b5a48d99276b875150055c9370610e6fb6c078
Jeff Bonhag <jbonhag@hashicorp.com>
2020-05-18 19:34:20 -0400

    clean up style

--- Casks/deadbeef.rb
+++ Casks/deadbeef.rb
@@ -2,7 +2,9 @@ cask 'deadbeef' do
   version '1.8.3'
   sha256 '9cd31ef07b7d1e8e597eed10fd62edc0699d3005f313a23a2bd26fa92b5115df'
 
+  # downloads.sourceforge.net was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/deadbeef/travis/osx/#{version}/deadbeef-#{version}-osx-x86_64.zip"
+  appcast 'https://sourceforge.net/projects/deadbeef/rss'
   name 'DeaDBeeF'
   homepage 'https://deadbeef.sourceforge.io/'
 
