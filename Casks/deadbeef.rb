ca48611c1b9947a758c3adc94195f6303ebf2c30
Jeff Bonhag <6937257+jbonhag@users.noreply.github.com>
2023-01-18 22:28:02 -0500

    Upgrade to latest versioned macOS release

--- Casks/deadbeef.rb
+++ Casks/deadbeef.rb
@@ -1,18 +1,18 @@
-cask 'deadbeef' do
-  version '1.8.3'
-  sha256 '9cd31ef07b7d1e8e597eed10fd62edc0699d3005f313a23a2bd26fa92b5115df'
+cask "deadbeef" do
+  version "1.9.5-beta2"
+  desc "a modular cross-platform audio player"
+  sha256 "6408cfdd1839b490544f9473a1aa771b2a0bee0e541662824fb277867856530a"
 
   # downloads.sourceforge.net was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/deadbeef/travis/osx/#{version}/deadbeef-#{version}-osx-x86_64.zip"
-  appcast 'https://sourceforge.net/projects/deadbeef/rss'
-  name 'DeaDBeeF'
-  homepage 'https://deadbeef.sourceforge.io/'
+  url "https://downloads.sourceforge.net/project/deadbeef/travis/macOS/1.9/deadbeef-#{version}-macos-universal.zip", verified: "downloads.sourceforge.net/project/deadbeef"
+  name "DeaDBeeF"
+  homepage "https://deadbeef.sourceforge.io/"
 
-  app 'DeaDBeeF.app'
+  app "DeaDBeeF.app"
 
   zap trash: [
-               '~/Library/Preferences/com.deadbeef.DeaDBeeF.plist',
-               '~/Library/Preferences/deadbeef',
-               '~/Library/Saved Application State/com.deadbeef.deadbeef.savedState',
-             ]
+    "~/Library/Preferences/com.deadbeef.DeaDBeeF.plist",
+    "~/Library/Preferences/deadbeef",
+    "~/Library/Saved Application State/com.deadbeef.deadbeef.savedState",
+  ]
 end
